6520a960ef2329c92757fa1793d0a0e3edb2f310
rkdms357 <79949390+rkdms357@users.noreply.github.com>
2021-03-13 13:27:32 +0900

    20210313

---
+++ Day1-5.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int a = scanner.nextInt();
+		scanner.close();
+		
+		for (int i = 0; i < a; i++) {
+			for (int j = a - 1; j > i; j--) {
+				System.out.print(" ");
+			}
+			for (int k = 0; k <= i; k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
